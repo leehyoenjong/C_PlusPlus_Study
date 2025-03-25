aeb0d4bbea9f2dc6c48ee40a6493f903d5279b97
leehyoenjong <99190990+leehyoenjong@users.noreply.github.com>
2025-03-26 06:51:15 +0900

    빙고 게임 완성

--- main1.cpp
+++ main1.cpp
@@ -24,6 +24,8 @@ int main()
 		iArray[idx2] = iTemp;
 	}
 
+	int iBingo = 0;
+
 	while (true)
 	{
 		system("cls");
@@ -44,10 +46,18 @@ int main()
 			cout << endl;
 		}
 
+		cout << "Bingo Line : " << iBingo << endl;
+		if (iBingo >= 2)
+		{
+			break;
+		}
+
+
+
 		cout << "숫자를 입력하세요(0 : 종료) : ";
 		int iInput;
 		cin >> iInput;
-		
+
 		//종료
 		if (iInput == 0)
 		{
@@ -67,7 +77,7 @@ int main()
 		for (int i = 0; i < 25; i++)
 		{
 			//같은 숫자가 있을 경우 
-			if (iInput == iArray[i]) 
+			if (iInput == iArray[i])
 			{
 				//숫자를 찾았을 경우 중복된 숫자가 아니므로 
 				//bAcc를 false로 만든다
@@ -86,6 +96,70 @@ int main()
 		if (bAcc)
 		{
 			continue;
+		}
+
+		//빙고 수 를 체크하는 것은 매번 숫자를 입력할때마다 처음부터 새로 카운트를 할 것이다
+		//그러므로 iBingo를 0으로 초기화 한다.
+		iBingo = 0;
+
+		//가로, 세로 줄 수를 구해준다
+		int iStar1 = 0, iStar2 = 0;
+		for (int i = 0; i < 5; i++)
+		{
+			//한줄 체크하기 전 먼저 0으로 초기화
+			iStar1 = iStar2 = 0;
+			for (int j = 0; j < 5; j++)
+			{
+				//가로 별 갯수 구해준다
+				if (iArray[i * 5 + j] == INT_MAX)
+				{
+					++iStar1;
+				}
+				//세로 별 개수	구해준다
+				if (iArray[j * 5 + i] == INT_MAX)
+				{
+					++iStar2;
+				}
+			}
+
+			//j for문이 끝났을 때 iStar1이 5라면 가로 한줄이 모두 *이라는 의미
+			if (iStar1 == 5)
+			{
+				++iBingo;
+			}
+			if (iStar2 == 5)
+			{
+				++iBingo;
+			}
+		}
+		//왼쪽 상단 -> 오른쪽 하단 대각선 체크
+		//대각선으로 6씩 증가
+		iStar1 = 0;
+		for (int i = 0; i < 25; i += 6)
+		{
+			if (iArray[i] == INT_MAX)
+			{
+				++iStar1;
+			}
+		}
+		if (iStar1 == 5)
+		{
+			++iBingo;
+		}
+
+		//오른쪽 상단 -> 왼쪽 하단 대각선 체크
+		//대각선으로 4씩 증가
+		iStar1 = 0;
+		for (int i = 4; i <= 20; i += 4)
+		{
+			if (iArray[i] == INT_MAX)
+			{
+				++iStar1;
+			}
+		}
+		if (iStar1 == 5)
+		{
+			++iBingo;
 		}
 	}
 
