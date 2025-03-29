da21a0fa9ccca834604b2ffd46d319cfec30fbc6
leehyoenjong <99190990+leehyoenjong@users.noreply.github.com>
2025-03-29 10:43:06 +0900

    TextRPG_Setting

--- main.cpp
+++ main.cpp
@@ -27,7 +27,8 @@ enum JOB
 	JOB_NONE,
 	JOB_KNIGHT,
 	JOB_ARCHER,
-	JOB_WIZARD
+	JOB_WIZARD,
+	JOB_END
 };
 
 #define NAME_SIZE 32
@@ -68,6 +69,132 @@ struct _tagMonster
 
 int main()
 {
+	srand((unsigned int)time(0));
+	//게임 시작할때 플레이어 정보 설정
+
+	_tagPlayer tPlayer = {};
+
+	//이름 설정
+	cout << "이름 : ";
+	//끝까지 문자열을 다 채워버리면 null을 인식 못하기 때문
+	cin.getline(tPlayer.strName, NAME_SIZE - 1);
+
+
+	//직업 설정 
+	int iJob = JOB_NONE;
+	while (iJob == JOB_NONE)
+	{
+		system("cls");
+		cout << "1. 기사" << endl;
+		cout << "2. 궁수" << endl;
+		cout << "3. 마법사" << endl;
+		cout << "직업을 선택하세요 : " << endl;
+
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(1024, '\n');
+			continue;
+		}
+		else if (iJob <= JOB_NONE || iJob >= JOB_END)
+		{
+			iJob = JOB_NONE;
+		}
+
+		//직업 이름
+		tPlayer.eJob = (JOB)iJob;
+
+		switch (tPlayer.eJob)
+		{
+		case JOB_KNIGHT:
+			strcpy_s(tPlayer.strJobNAME, "기사");
+			tPlayer.iAttackMin = 5;
+			tPlayer.iAttackMax = 10;
+			tPlayer.iArmorMin = 15;
+			tPlayer.iArmorMin = 20;
+			tPlayer.iHpMax = 500;
+			tPlayer.iHp = 500;
+			tPlayer.iMP = 100;
+			tPlayer.iMPMax = 100;
+			break;
+		case JOB_ARCHER:
+			strcpy_s(tPlayer.strJobNAME, "궁수");
+			tPlayer.iAttackMin = 10;
+			tPlayer.iAttackMax = 15;
+			tPlayer.iArmorMin = 10;
+			tPlayer.iArmorMin = 15;
+			tPlayer.iHpMax = 400;
+			tPlayer.iHp = 400;
+			tPlayer.iMP = 200;
+			tPlayer.iMPMax = 200;
+			break;
+		case JOB_WIZARD:
+			strcpy_s(tPlayer.strJobNAME, "마법사");
+			tPlayer.iAttackMin = 15;
+			tPlayer.iAttackMax = 20;
+			tPlayer.iArmorMin = 5;
+			tPlayer.iArmorMin = 10;
+			tPlayer.iHpMax = 300;
+			tPlayer.iHp = 300;
+			tPlayer.iMP = 300;
+			tPlayer.iMPMax = 300;
+			break;
+		}
+
+		tPlayer.iLevel = 1;
+		tPlayer.iExp = 0;
+	}
+
+	//몬스터 생성
+	_tagMonster tMonsterArr[MT_BACK - 1] = {};
+
+	//고블린 생성
+	strcpy_s(tMonsterArr[0].strName, "고블린");
+	tMonsterArr[0].iAttackMin = 20;
+	tMonsterArr[0].iAttackMin = 30;
+	tMonsterArr[0].iArmorMin = 2;
+	tMonsterArr[0].iArmorMax = 5;
+	tMonsterArr[0].iHp = 100;
+	tMonsterArr[0].iHpMax = 100;
+	tMonsterArr[0].iMP = 10;
+	tMonsterArr[0].iMPMax = 10;
+	tMonsterArr[0].iLevel = 1;
+	tMonsterArr[0].iExp = 1000;
+	tMonsterArr[0].iGoleMin = 500;
+	tMonsterArr[0].iGoleMax = 1500;
+
+	//트롤 생성
+	strcpy_s(tMonsterArr[1].strName, "트롤");
+	tMonsterArr[1].iAttackMin = 80;
+	tMonsterArr[1].iAttackMin = 130;
+	tMonsterArr[1].iArmorMin = 60;
+	tMonsterArr[1].iArmorMax = 90;
+	tMonsterArr[1].iHp = 2000;
+	tMonsterArr[1].iHpMax = 2000;
+	tMonsterArr[1].iMP = 100;
+	tMonsterArr[1].iMPMax = 100;
+	tMonsterArr[1].iLevel = 5;
+	tMonsterArr[1].iExp = 7000;
+	tMonsterArr[1].iGoleMin = 6000;
+	tMonsterArr[1].iGoleMax = 8000;
+
+	//드래곤 생성
+	strcpy_s(tMonsterArr[2].strName, "드래곤");
+	tMonsterArr[2].iAttackMin = 250;
+	tMonsterArr[2].iAttackMin = 500;
+	tMonsterArr[2].iArmorMin = 200;
+	tMonsterArr[2].iArmorMax = 400;
+	tMonsterArr[2].iHp = 30000;
+	tMonsterArr[2].iHpMax = 30000;
+	tMonsterArr[2].iMP = 20000;
+	tMonsterArr[2].iMPMax = 20000;
+	tMonsterArr[2].iLevel = 10;
+	tMonsterArr[2].iExp = 30000;
+	tMonsterArr[2].iGoleMin = 20000;
+	tMonsterArr[2].iGoleMax = 50000;
+
+	
+
 	while (true)
 	{
 		//로비 구성
@@ -121,6 +248,27 @@ int main()
 				if (imenu == MT_BACK)
 				{
 					break;
+				}
+
+				//선택한 메뉴 1을 빼주면 몬스터 배열의 인덱스가 된다
+				//그렇게 해서 해당 맵의 몬스터 생성
+				_tagMonster tMonster = tMonsterArr[imenu - 1];
+
+				//전투 시작
+				while (true)
+				{
+					switch (imenu)
+					{
+					case MT_EASY:
+						cout << "********* 쉬움 *********" << endl;
+						break;
+					case MT_NORMAL:
+						cout << "********* 보통 *********" << endl;
+						break;
+					case MT_HARD:
+						cout << "********* 어려움 *********" << endl;
+						break;
+					}
 				}
 			}
 			break;
